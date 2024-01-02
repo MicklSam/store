e9d15807c7ebff870e9e7831b87219ae839703ad
reem712 <reemmaher712@gmail.com>
2024-01-02 07:40:18 +0200

    0

--- control.php
+++ control.php
@@ -2,11 +2,10 @@
 echo $_SERVER ["REQUEST_METHOD"]. ' request', '<br>';
 if ($_SERVER ["REQUEST_METHOD"]=="POST")
 {
-
-echo "user email is". $_POST['emaill' ]. "<br>";
-echo "passovwd is ".$_POST ['pass' ]."<br>";
+echo "user email is". $_POST['emaill']. "<br>";
+echo "passovwd is ".$_POST ['pass']."<br>";
 Sadmin=array ("asmaa", "salma", "reem");
-$name=$_POST [ 'emaill' ];
+$name=$_POST ['emaill'];
 if (in_array ($name , $admin))
 {
 echo "hi". $name ."<br>";
